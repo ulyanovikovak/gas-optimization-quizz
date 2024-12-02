0280eb2763f6b9d3b8b58422ac3f43176ad86a64
ulianovikova <yasenevsk.18@gmail.com>
2024-12-02 14:34:46 +0300

    08

--- src/08. ArrayType.sol
+++ src/08. ArrayType.sol
@@ -14,7 +14,12 @@ contract ArrayType is IArrayType {
 }
 
 contract ArrayTypeOptimized is IArrayType {
-    /* YOUR SOLUTION GOES HERE */
+    uint256[200] array;
 
-    function initArray() public {}
+    function initArray() public {
+        for (uint256 i = 0; i < 200; ++i) {
+            array[i] = i;
+        }
+    }
+
 }
